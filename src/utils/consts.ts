7e39d0994672cd11b529a36c7a9c22e2054b5995
Carlos de Lima Junior <cj.moyses@gmail.com>
2024-10-02 10:45:02 -0300

    chore: add firebase collections name into consts file

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -5,6 +5,8 @@ const storeDomain: string = process.env.SHOPIFY_STORE_URL!;
 const apiVersion: string = "2022-04";
 
 export const PORT: string = process.env.PORT ?? "3000";
+export const COLLECTION_1: string = process.env.FIREBASE_COLLECTION_1!;
+export const COLLECTION_2: string = process.env.FIREBASE_COLLECTION_2!;
 
 export const clientParams = {
   storeDomain,
